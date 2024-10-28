6851bf7b722e847fecc20777970a6488376d4d7e
kyoungchan <dlsrks1021@gmail.com>
2024-10-29 00:32:33 +0900

    fix: use completableFuture allof method to run after async methods hasRole

--- src/main/java/com/movinfo/messenger/handler/SelectMenuHandler.java
+++ src/main/java/com/movinfo/messenger/handler/SelectMenuHandler.java
@@ -2,18 +2,15 @@ package com.movinfo.messenger.handler;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import com.movinfo.messenger.command.RoleManager;
-import com.movinfo.messenger.model.Movie;
 import com.movinfo.messenger.model.Screen;
 import com.movinfo.messenger.util.JDAUtils;
-import com.movinfo.messenger.util.MongoUtils;
-
 import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.components.selections.SelectOption;
 import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
-import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction;
 
 public class SelectMenuHandler extends ListenerAdapter{
 
@@ -23,11 +20,12 @@ public class SelectMenuHandler extends ListenerAdapter{
     private void handleMovieSelectMenu(StringSelectInteractionEvent event){
         String selectedMovieName = event.getValues().get(0);
 
+        List<CompletableFuture<Void>> futures = new LinkedList<>();
         List<SelectOption> selectOptions = new LinkedList<>();
         for (String type : Screen.SCREEN_TYPE_LIST){
             String roleName = selectedMovieName+"_"+type;
 
-            RoleManager.hasRole(JDAUtils.getGuild(), roleName, event.getUser()).thenAccept(hasRole -> {
+            CompletableFuture<Void> future = RoleManager.hasRole(JDAUtils.getGuild(), roleName, event.getUser()).thenAccept(hasRole -> {
                 SelectOption option;
                 if (hasRole){
                     option = SelectOption.of(type, roleName).withDefault(true);
@@ -37,18 +35,22 @@ public class SelectMenuHandler extends ListenerAdapter{
 
                 selectOptions.add(option);
             });
+
+            futures.add(future);
         }
 
-        StringSelectMenu menu = StringSelectMenu.create("screen-select-menu-"+selectedMovieName)
-            .setPlaceholder("상영관 종류")
-            .setMaxValues(6)
-            .setMinValues(0)
-            .addOptions(selectOptions)
-            .build();
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).thenRun(() -> {
+            StringSelectMenu menu = StringSelectMenu.create("screen-select-menu-"+selectedMovieName)
+                .setPlaceholder("상영관 종류")
+                .setMaxValues(6)
+                .setMinValues(0)
+                .addOptions(selectOptions)
+                .build();
             
-        event.reply("[" + selectedMovieName + "] 영화에 대해 알림을 설정할 극장 종류를 선택해주세요.").setEphemeral(true)
-            .addActionRow(menu)
-            .queue();
+            event.reply("[" + selectedMovieName + "] 영화에 대해 알림을 설정할 극장 종류를 선택해주세요.").setEphemeral(true)
+                .addActionRow(menu)
+                .queue();
+        });
     }
 
     private void handleScreenSelectMenu(StringSelectInteractionEvent event){
